21828a4c5eba128211c5c24c95a4d67c2c2344db
AnishaKB1 <anishakb4@gmail.com>
2023-11-22 22:45:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const cors = require('cors');
+const mongoose=require('mongoose');
+const path=require('path');
+const app=new express();
+
+const employeedata = require('./model/employee');
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+require("./db/connect");
+
+const emprouter=require('./routes/basic');
+app.use('/emp',emprouter);
+
+
+
+//port number
+  
+  const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+})
